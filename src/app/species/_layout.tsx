14b6ebec8a4087802e2487b6c32615f6179289a0
palcchi <valliantito99@gmail.com>
2026-09-24 19:32:33 +0700

    Align species layout with LANJUTAN module

--- src/app/species/_layout.tsx
+++ src/app/species/_layout.tsx
@@ -1,19 +1,17 @@
 import { Stack } from 'expo-router';
-import React from 'react';
 import { Colors } from '../../constants/Colors';
 
 export default function SpeciesLayout() {
   return (
     <Stack
       screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary },
-        headerTintColor: Colors.surface,
-        headerTitleStyle: { fontWeight: 'bold' },
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: Colors.background,
+        },
       }}
     >
-      <Stack.Screen name="index" options={{ title: 'Spesies' }} />
-      <Stack.Screen name="[id]" options={{ headerShown: false }} />
-      <Stack.Screen name="search" options={{ title: 'Pencarian Spesies' }} />
+      <Stack.Screen name="[id]" />
     </Stack>
   );
 }
